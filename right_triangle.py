f7e0b77901255875257ac78f6b8d3b97b50f5d85
arpanhere <149156101+arpanhere@users.noreply.github.com>
2026-02-07 23:56:30 +0530

    Delete right_triangle.py

--- right_triangle.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter the no. of rows: "))
-for i in range(n):
-    for j in range(n):
-        if i ==j or i == n-1 or i>=j:
-            print("*", end = ' ')
-        else:
-            print("", end = "")
-    print( )
